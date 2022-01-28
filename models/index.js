e44f5eee674f1607669662600b31c9c722c50a42
Kakashi <nickperel@live.com>
2022-01-28 18:01:21 -0500

    finished the category and product models and established their associations with each other

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+// const Tag = require('./Tag');
+// const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
@@ -15,6 +19,6 @@ const ProductTag = require('./ProductTag');
 module.exports = {
   Product,
   Category,
-  Tag,
-  ProductTag,
+  // Tag,
+  // ProductTag,
 };
